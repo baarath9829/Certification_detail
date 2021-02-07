f436bfd84582166994d462ba6c90426a0cadafff
baarath <baarathsrinivas@gmail.com>
2021-02-08 01:01:23 +0530

    added success page

---
+++ src/component/Success.js
@@ -0,0 +1,10 @@
+import './Register.css';
+const success = () => {
+    return (
+        <div className='Success'>
+            <h1 className='text-center'> successfully registered </h1> 
+            <h1 className='text-center'>check your mail for further instruction</h1>
+        </div>
+    );
+}
+export default success
